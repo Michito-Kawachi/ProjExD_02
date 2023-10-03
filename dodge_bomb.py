375f08756e2d24ee68510292edc735426cc53054
michito_K <c0b2204560@edu.teu.ac.jp>
2023-10-03 14:07:01 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,8 +14,12 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     bomb = pg.Surface((20, 20))
-    bb_x = randint(0, WIDTH)
-    bb_y = randint(0, HEIGHT)
+    bb_x: int = randint(0, WIDTH)
+    bb_y: int = randint(0, HEIGHT)
+    vx: int = 5
+    vy: int = 5
+    bb_rct = bomb.get_rect()
+    bb_rct.center = bb_x, bb_y
     pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
     bomb.set_colorkey("black")
 
@@ -27,10 +31,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bomb, [bb_x, bb_y])
+        screen.blit(bomb, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        bb_rct.move_ip(vx, vy)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
